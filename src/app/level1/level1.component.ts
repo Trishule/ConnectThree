5a9d10935fd229fdb331ae7e56acf447861797a3
Trishul Gurumurthy <gurumruthytrishul078@gmail.com>
2022-11-06 13:28:22 -0600

    Update level1.component.ts

--- src/app/level1/level1.component.ts
+++ src/app/level1/level1.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+/**
+ * Component
+ */
+
+/**
+ * Decorator
+ */
+
 
 @Component({
+
   selector: 'app-level1',
   templateUrl: './level1.component.html',
   styleUrls: ['./level1.component.css']
 })
+
+
 export class Level1Component implements OnInit {
 
+  /**
+   * Creates an instance of level1 component.
+   */
   constructor() { }
 
+
+  /**
+   * on init
+   */
   ngOnInit(): void {
   }
 
