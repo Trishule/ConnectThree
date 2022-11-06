d53af037f02bdf4eae950f0c2c077c9beed5190d
Trishul Gurumurthy <gurumruthytrishul078@gmail.com>
2022-11-06 17:12:04 -0600

    Update level2.component.ts

--- src/app/level2/level2.component.ts
+++ src/app/level2/level2.component.ts
@@ -9,7 +9,9 @@ export class Level2Component  {
   private currentPlayerIx;
   public boardContent: number[][];
   private currentWinnerIx: number;
+  private playerNames: string[];
   constructor() {
+    this.playerNames = ['','X','O'];
     this.boardContent = [
       [0,0,0],
       [0,0,0],
@@ -18,5 +20,27 @@ export class Level2Component  {
 
     this.currentPlayerIx = 1;
     this.currentWinnerIx = 0;
+
+  }
+
+  public getPlayerName(col: number, row:number): string {
+    return this.playerNames[this.boardContent[row][col]];
+  }
+
+  public getStyle(col: number, row: number): string {
+    if(this.boardContent[row][col] !== 0) {
+    return `occupied-${this.getPlayerName(col,row)}`
+    }
+
+    return '';
+  }
+
+  public set(col: number, row: number): void {
+    if (this.currentWinnerIx === 0 && this.boardContent[row][col] === 0) {
+      this.boardContent[row][col] = this.currentPlayerIx;
+      this.currentPlayerIx = this.currentPlayerIx === 1 ? 2 : 1;
+
+
+    }
   }
 }
